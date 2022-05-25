a58f3f432b5006595293f6c17a0d930e9ffc524d
Pedro Santayana <pedrosantayana@gmail.com>
2022-05-25 20:32:00 -0300

    manual merge

--- src/parser.rs
+++ src/parser.rs
@@ -13,7 +13,114 @@ impl AST {
 struct Chunk;
 
 
+use nom::{
+    IResult,
+    bytes::complete::{tag, take_while_m_n, take_until},
+    combinator::map_res,
+    sequence::tuple,
+    character::complete::{
+      one_of,
+      char,
+    },
+    bytes::complete::is_not,
+    
+  };
 
+
+
+enum Grammar {
+    Chuck(Box<crate::parser::Grammar>),
+    Block {
+        stat: Vec<Statement>,
+        retstat: Option<ReturnStatement>,
+    },
+    
+}
+
+struct Chunk {
+    block: Block,
+}
+
+struct Block {
+    stat: Vec<Statement>,
+    retstat: Option<ReturnStatement>,
+}
+
+enum Statement {
+    SemiColon,
+    VariableAttribution {
+        varlist: Vec<Variable>,
+        explist: Vec<Expression>,
+    },
+    FunctionCall(FunctionCall),
+    Label(Label),
+    Break,
+    GoTo{
+        exp: Expression,
+        block: Block
+    },
+    Do {
+        block: Block,
+        exp: Expression
+    },
+    While {
+        exp: Expression,
+        block: Block
+    },
+    Repeat {
+        block: Block,
+        exp: Expression
+    },
+    If {
+        exp: Expression,
+        block: Block,
+        elseif: Vec<ElseIf>,
+        elseblock: Option<Block>
+    },
+    For {
+        var: Name,
+        initial: Expression,
+        limit: Expression,
+        step: Option<Expression>,
+        block: Block
+    },
+    ForIn {
+        namelist: Vec<Name>,
+        explist: Vec<Expression>,
+        block: Block
+    },
+    Function {
+        funcname: FuncName,
+        funcbody: FunctionBody
+    },
+    FunctionDeclaration {
+        funcname: Name,
+        funcbody: FunctionBody
+    },
+    ListAttribution {
+        attnamelist: AttributeNameList,
+        explist: Option<Vec<Expression>>,
+    }
+}
+
+struct AttributeNameList {
+    name: Name,
+    attrib: Attribute,
+    attnamelist: Vec<AttributeNameList>,
+}
+
+
+
+// --------------------------------
+
+
+struct ReturnStatement {";")(input)?;
+        let (input, _) = tag(";")(input)?;
+        ReturnStatement {
+            
+        }
+    }
+}
 
 
 
