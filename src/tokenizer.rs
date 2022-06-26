7f69115194318023a61368148c46e474611857c7
Pedro Santayana <pedrosantayana@gmail.com>
2022-06-26 20:05:14 -0300

    add tokenizer.rs

---
+++ src/tokenizer.rs
@@ -0,0 +1,25 @@
+struct Tokenizer;
+
+struct TokenStream(Vec<Token>);
+
+struct 
+
+impl Tokenizer {
+    pub fn from_string(s: &str) -> Result<TokenStream, Error> {
+
+    }
+}
+
+struct Token {
+    token_type: TokenType,
+    value: &str
+}
+
+enum TokenType {
+
+}
+
+#[test]
+fn tokenizer() {
+    let tokenizer = 
+}
