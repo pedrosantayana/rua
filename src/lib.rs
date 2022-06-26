914323e7158d98cb99d8ac3651698698617ff898
Pedro Santayana <pedrosantayana@gmail.com>
2022-06-26 20:06:20 -0300

    clear tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,10 @@
-mod parser;
+mod old_parser;
+
 
 #[cfg(test)]
 mod tests {
-    pub use crate::parser::AST;
-    
     #[test]
     fn test_var() {
-        let ast = AST::from_string("local varName1 = 100");
+        
     }
-
-
 }
