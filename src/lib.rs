adec26602f6f50612ed7d96d517ecb0636f6afca
Pedro Santayana <pedrosantayana@gmail.com>
2022-09-19 08:06:52 -0300

    add pair test

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,1 @@
 mod parser;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn test_var() {
-        let a = parser::LuaParser::parse();
-    }
-}
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,5 +1,26 @@
-use pest_derive::Parser;
 
-#[derive(Parser)]
+use pest::Parser;
+
+
+
+#[derive(pest_derive::Parser)]
 #[grammar = "parser/lua.pest"]
 pub struct LuaParser;
+
+#[test]
+fn create_parser() {
+  
+  let file = LuaParser::parse(Rule::chunk, "goto a")
+  .expect("unsuccessful parse") // unwrap the parse result
+  .next().unwrap();
+  
+  for line in file.into_inner() {
+    match line.as_rule() {
+        Rule::stat => {
+            println!("{}", line.as_str());
+        }
+        Rule::EOI => (),
+        _ => unreachable!(),
+    }
+  }
+}
