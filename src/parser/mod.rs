4cc1b1e6273900d46b3c8d3e7124d7d3b379057d
Pedro Santayana <pedrosantayana@gmail.com>
2022-12-20 18:07:18 -0300

    todo: parser

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,7 +1,4 @@
-
 use pest::Parser;
-
-
 
 #[derive(pest_derive::Parser)]
 #[grammar = "parser/lua.pest"]
@@ -9,18 +6,18 @@ pub struct LuaParser;
 
 #[test]
 fn create_parser() {
-  
-  let file = LuaParser::parse(Rule::chunk, "goto a")
-  .expect("unsuccessful parse") // unwrap the parse result
-  .next().unwrap();
-  
-  for line in file.into_inner() {
-    match line.as_rule() {
-        Rule::stat => {
-            println!("{}", line.as_str());
+    let file = LuaParser::parse(Rule::chunk, "goto a")
+        .expect("unsuccessful parse") // unwrap the parse result
+        .next()
+        .unwrap();
+
+    for line in file.into_inner() {
+        match line.as_rule() {
+            Rule::stat => {
+                println!("{}", line.as_str());
+            }
+            Rule::EOI => (),
+            _ => unreachable!(),
         }
-        Rule::EOI => (),
-        _ => unreachable!(),
     }
-  }
 }
